b69ca8b478a21615d18f8d7b8f553aa2e54abf0f
azu-caylent <charles.ikenyei@caylent.com>
2022-09-26 19:52:38 -0400

    Adding caching

--- build/git_update.sh
+++ build/git_update.sh
@@ -12,7 +12,7 @@ done
 
 # get highest tag number, and add v0.1.0 if doesn't exist
 git fetch --prune --unshallow 2>/dev/null
-CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
+CURRENT_VERSION=$(git describe --abbrev=0 --tags 2>/dev/null)
 
 if [[ $CURRENT_VERSION == '' ]]
 then

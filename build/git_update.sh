b5f0b89bd721016ebd11559c34ecbcf753fecdc6
azu-caylent <charles.ikenyei@caylent.com>
2022-09-26 20:04:17 -0400

    Test

--- build/git_update.sh
+++ build/git_update.sh
@@ -12,12 +12,11 @@ done
 
 # get highest tag number, and add v0.1.0 if doesn't exist
 git fetch --prune --unshallow 2>/dev/null
-CURRENT_VERSION=$(git describe --abbrev=0 --tags 2>/dev/null)
+CURRENT_VERSION=`git describe --abbrev=0 --tags 2>/dev/null`
 
 if [[ $CURRENT_VERSION == '' ]]
 then
-  # CURRENT_VERSION='x.x.x'
-  CURRENT_VERSION='1.0.0'
+  CURRENT_VERSION='x.x.x'
 fi
 echo "Current Version: $CURRENT_VERSION"
 
@@ -48,8 +47,8 @@ NEW_TAG="$VNUM1.$VNUM2.$VNUM3"
 echo "($VERSION) updating $CURRENT_VERSION to $NEW_TAG"
 
 # get current hash and see if it already has a tag
-GIT_COMMIT=$(git rev-parse HEAD)
-NEEDS_TAG=$(git describe --contains $GIT_COMMIT 2>/dev/null)
+GIT_COMMIT=`git rev-parse HEAD`
+NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
 # only tag if no tag already
 if [ -z "$NEEDS_TAG" ]; then
